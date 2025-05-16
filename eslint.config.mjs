20b6554586512e6db4e6279d016a1b8996cdf384
tallen2828 <tallen2828@gmail.com>
2025-05-16 16:02:02 -0600

    add rule plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import typescriptParser from "@typescript-eslint/parser";
+import typescriptPlugin from "@typescript-eslint/eslint-plugin";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -12,73 +14,45 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.config({
     extends: ["next"],
+  }),
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    plugins: {
+      "@typescript-eslint": typescriptPlugin,
+    },
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+    },
     rules: {
       "@typescript-eslint/no-explicit-any": [
         "error",
         {
           ignoreRestArgs: true,
-          ignorePatterns: ["src/generated/**/*"],
         },
       ],
-      "@typescript-eslint/no-unused-vars": [
-        "error",
-        {
-          ignorePatterns: ["src/generated/**/*"],
-        },
-      ],
-      "@typescript-eslint/no-empty-object-type": [
-        "error",
-        {
-          ignorePatterns: ["src/generated/**/*"],
-        },
-      ],
-      "@typescript-eslint/no-unused-expressions": [
-        "error",
-        {
-          ignorePatterns: ["src/generated/**/*"],
-        },
-      ],
-      "@typescript-eslint/no-this-alias": [
-        "error",
-        {
-          ignorePatterns: ["src/generated/**/*"],
-        },
-      ],
-      "@typescript-eslint/no-require-imports": [
-        "error",
-        {
-          ignorePatterns: ["src/generated/**/*"],
-        },
-      ],
-      "@typescript-eslint/no-wrapper-object-types": [
-        "error",
-        {
-          ignorePatterns: ["src/generated/**/*"],
-        },
-      ],
-      "@typescript-eslint/no-unsafe-function-type": [
-        "error",
-        {
-          ignorePatterns: ["src/generated/**/*"],
-        },
-      ],
+      "@typescript-eslint/no-unused-vars": ["error"],
+      "@typescript-eslint/no-empty-object-type": ["error"],
+      "@typescript-eslint/no-unused-expressions": ["error"],
+      "@typescript-eslint/no-this-alias": ["error"],
+      "@typescript-eslint/no-require-imports": ["error"],
+      "@typescript-eslint/no-unsafe-function-type": ["error"],
     },
-    overrides: [
-      {
-        files: ["src/generated/**/*"],
-        rules: {
-          "@typescript-eslint/no-explicit-any": "off",
-          "@typescript-eslint/no-unused-vars": "off",
-          "@typescript-eslint/no-empty-object-type": "off",
-          "@typescript-eslint/no-unused-expressions": "off",
-          "@typescript-eslint/no-this-alias": "off",
-          "@typescript-eslint/no-require-imports": "off",
-          "@typescript-eslint/no-wrapper-object-types": "off",
-          "@typescript-eslint/no-unsafe-function-type": "off",
-        },
-      },
-    ],
-  }),
+  },
+  {
+    files: ["src/generated/**/*"],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
+      "@typescript-eslint/no-unused-expressions": "off",
+      "@typescript-eslint/no-this-alias": "off",
+      "@typescript-eslint/no-require-imports": "off",
+      "@typescript-eslint/no-unsafe-function-type": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
